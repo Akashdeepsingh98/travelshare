0975baad9046ec0c0fd906ca0da9f5923f6cbfe8
Akashdeep <akashdeepsingh98@gmail.com>
2025-06-28 14:21:08 +0530

    Revert "Fix GroupChatsTab Supabase query ambiguity"
    This reverts commit 12864616e63463c4cad73a0816fd36a6dad38135.

--- src/components/GroupChatsTab.ts
+++ src/components/GroupChatsTab.ts
@@ -67,7 +67,7 @@ export function createGroupChatsTab(
               name,
               avatar_url
             ),
-            last_message:group_messages!fk_last_message(
+            last_message:group_messages(
               id,
               content,
               sender_id,
